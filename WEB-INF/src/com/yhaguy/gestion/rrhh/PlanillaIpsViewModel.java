4c8f7bfe3c1f652c0d00dc7b95b8106a7376461c
sraul <sergioraul777@gmail.com>
2023-10-31 11:24:38 -0400

    planilla ips

--- WEB-INF/src/com/yhaguy/gestion/rrhh/PlanillaIpsViewModel.java
+++ WEB-INF/src/com/yhaguy/gestion/rrhh/PlanillaIpsViewModel.java
@@ -1,14 +1,21 @@
 package com.yhaguy.gestion.rrhh;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.zkoss.bind.annotation.AfterCompose;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.Init;
 import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.util.media.AMedia;
+import org.zkoss.zul.Filedownload;
 
 import com.coreweb.control.SimpleViewModel;
 import com.yhaguy.domain.RRHHPlanillaSalarios;
@@ -55,6 +62,75 @@ public class PlanillaIpsViewModel extends SimpleViewModel {
 		}
 	}
 	
+	@Command
+	public void exportExcel() throws Exception {
+		Workbook workbook = new HSSFWorkbook();
+		Sheet listSheet = workbook.createSheet("Planilla Salarios");
+
+		int rowIndex = 0;
+		Row r = listSheet.createRow(rowIndex++);
+		int cell = 0;
+		r.createCell(cell++).setCellValue("CEDULA");
+		r.createCell(cell++).setCellValue("APELLIDOS Y NOMBRES");
+		r.createCell(cell++).setCellValue("DIAS TRABAJADOS");
+		r.createCell(cell++).setCellValue("BONIF.FLIAR");
+		r.createCell(cell++).setCellValue("COMISION");
+		r.createCell(cell++).setCellValue("SALARIO");
+		r.createCell(cell++).setCellValue("TOTAL");
+		r.createCell(cell++).setCellValue("IPS 9%");
+		
+		double comisiones = 0.0;
+		double salarios = 0.0;
+		double totales = 0.0;
+		double ips = 0.0;	
+		double bonif = 0.0;
+		
+		for (BeanIps p : this.getPlanillas()) {
+			Row row = listSheet.createRow(rowIndex++);
+			int cellIndex = 0;
+			row.createCell(cellIndex++).setCellValue(p.getCedula() + ""); 
+			row.createCell(cellIndex++).setCellValue(p.getFuncionario() + "");
+			row.createCell(cellIndex++).setCellValue(Utiles.getRedondeo(p.getDiasTrabajados())); 
+			row.createCell(cellIndex++).setCellValue(Utiles.getRedondeo(p.getBonificacion()));
+			row.createCell(cellIndex++).setCellValue(Utiles.getRedondeo(p.getComision()));
+			row.createCell(cellIndex++).setCellValue(Utiles.getRedondeo(p.getSalario()));
+			row.createCell(cellIndex++).setCellValue(Utiles.getRedondeo(p.getTotal()));
+			row.createCell(cellIndex++).setCellValue(Utiles.getRedondeo(p.getIps()));
+			
+			comisiones += p.getComision();
+			salarios += p.getSalario();
+			totales += p.getTotal();
+			ips += p.getIps();
+			bonif += p.getBonificacion();
+		}
+		
+		Row t = listSheet.createRow(rowIndex++);
+		int cell_ = 0;
+		t.createCell(cell_++).setCellValue("TOTALES");
+		t.createCell(cell_++).setCellValue("");
+		t.createCell(cell_++).setCellValue("");
+		t.createCell(cell_++).setCellValue(Utiles.getRedondeo(bonif));
+		t.createCell(cell_++).setCellValue(Utiles.getRedondeo(comisiones));
+		t.createCell(cell_++).setCellValue(Utiles.getRedondeo(salarios));
+		t.createCell(cell_++).setCellValue(Utiles.getRedondeo(totales));
+		t.createCell(cell_++).setCellValue(Utiles.getRedondeo(ips));
+		
+		listSheet.autoSizeColumn(0); listSheet.autoSizeColumn(1);
+		listSheet.autoSizeColumn(2); listSheet.autoSizeColumn(3);
+		listSheet.autoSizeColumn(4); listSheet.autoSizeColumn(5);
+		listSheet.autoSizeColumn(6); listSheet.autoSizeColumn(7);
+		
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			workbook.write(baos);
+			AMedia amedia = new AMedia("PlanillaIPS.xls", "xls", "application/file", baos.toByteArray());
+			Filedownload.save(amedia);
+			baos.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	/**
 	 * @return las planillas..
 	 */
@@ -77,14 +153,16 @@ public class PlanillaIpsViewModel extends SimpleViewModel {
 		double comisiones = 0.0;
 		double salarios = 0.0;
 		double totales = 0.0;
-		double ips = 0.0;		
+		double ips = 0.0;	
+		double bonif = 0.0;
 		for (BeanIps item : this.planillas) {
 			comisiones += item.getComision();
 			salarios += item.getSalario();
 			totales += item.getTotal();
 			ips += item.getIps();
+			bonif += item.getBonificacion();
 		}		
-		return new Double[] { comisiones, salarios, totales, ips, totales * 0.255 };
+		return new Double[] { comisiones, salarios, totales, ips, bonif, totales * 0.255 };
 	}
 
 	public List<String> getMeses() {
